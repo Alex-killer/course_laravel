0d41b22cf77a986f85fc2e1ab00b55d19ee2c6be
alex-killer <onefrank@bk.ru>
2022-03-09 22:48:32 +0700

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        $posts = Post::where('is_published', 1)->get();
+        foreach ($posts as $post){
+            dump($post->title);
+        }
+        dd('end');
+    }
+}
