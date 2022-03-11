5e668b7d2aad78c9f4414ce1ca3f854298441be0
alex-killer <onefrank@bk.ru>
2022-03-11 21:32:11 +0700

    1

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,17 +5,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-<div>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{ route('main.index') }}">Main</a></li>
-                <li><a href="{{ route('post.index') }}">Post</a></li>
-                <li><a href="{{ route('contact.index') }}">Contact</a></li>
-            </ul>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('post.index') }}">Post</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contact.index') }}">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </div>
     @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/main', 'MainController@index')->name('main.index');
 Route::get('/post', 'PostController@index')->name('post.index');
+Route::get('/post/create', 'PostController@create')->name('post.create');
+Route::post('/post', 'PostController@store')->name('post.store');
+Route::get('/post/{post}', 'PostController@show')->name('post.show');
+Route::get('/post/{post}/edit', 'PostController@edit')->name('post.edit');
+Route::patch('/post/{post}', 'PostController@update')->name('post.update');
+Route::delete('/post/{post}', 'PostController@destroy')->name('post.delete');
+
+
 Route::get('/contact', 'ContactController@index')->name('contact.index');
 
